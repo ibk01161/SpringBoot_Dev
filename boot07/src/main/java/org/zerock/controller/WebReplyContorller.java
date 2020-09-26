02339eec5daa20d10fe635526ffd5fe781678ec6
ibk01161 <rkxmsdk@naver.com>
2020-09-26 22:53:41 +0900

    Add Project [boot7]_3

--- boot07/src/main/java/org/zerock/controller/WebReplyContorller.java
+++ boot07/src/main/java/org/zerock/controller/WebReplyContorller.java
@@ -1,5 +1,9 @@
 package org.zerock.controller;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.WebBoard;
 import org.zerock.domain.WebReply;
 import org.zerock.persistence.WebReplyRepository;
 
@@ -22,8 +27,9 @@ public class WebReplyContorller {
 	private WebReplyRepository replyrepos;
 	
 	// 댓글 등록
+	@Transactional
 	@PostMapping("/{bno}")
-	public ResponseEntity<Void> addReply(@PathVariable("bno") Long bno, @RequestBody WebReply reply) {
+	public ResponseEntity<List<WebReply>> addReply(@PathVariable("bno") Long bno, @RequestBody WebReply reply) {
 		
 		log.info("========================================");
 		log.info("addReply.....................");
@@ -31,8 +37,25 @@ public class WebReplyContorller {
 		log.info("addReply()_reply : " + reply);
 		log.info("========================================");
 		
-		return new ResponseEntity<>(HttpStatus.CREATED);
+		WebBoard board = new WebBoard();
+		board.setBno(bno);
+		reply.setBoard(board);
+		
+		replyrepos.save(reply);
+		
+		return new ResponseEntity<>(getListByBoard(board), HttpStatus.CREATED);
 		
 	}
+
+	// 댓글 리스트 조회
+	private List<WebReply> getListByBoard(WebBoard board) throws RuntimeException {
+		
+		log.info("getListByBoard....." + board);
+		
+		return replyrepos.getRepliesOfBoard(board);
+	}
+	
+	
+	
 
 }
