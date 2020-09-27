49f0fa9115095c4de8b996c353512dac0614b5cd
ibk01161 <rkxmsdk@naver.com>
2020-09-27 21:42:57 +0900

    Add Project [boot7]_4

--- boot07/src/main/java/org/zerock/controller/WebReplyContorller.java
+++ boot07/src/main/java/org/zerock/controller/WebReplyContorller.java
@@ -7,8 +7,11 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,8 +49,63 @@ public class WebReplyContorller {
 		return new ResponseEntity<>(getListByBoard(board), HttpStatus.CREATED);
 		
 	}
+	
+	// 댓글 삭제
+	@Transactional
+	@DeleteMapping("/{bno}/{rno}")
+	public ResponseEntity<List<WebReply>> remove(@PathVariable("bno") Long bno, @PathVariable("rno") Long rno) {
+		
+		log.info("========================================");
+		log.info("delete reply : " + rno);
+		log.info("========================================");
+		
+		replyrepos.deleteById(rno);
+		
+		WebBoard board = new WebBoard();
+		board.setBno(bno);
+		
+		return new ResponseEntity<>(getListByBoard(board), HttpStatus.OK);
+		
+	}
+	
+	// 댓글 수정
+	@Transactional
+	@PutMapping("/{bno}")
+	public ResponseEntity<List<WebReply>> modify(@PathVariable("bno") Long bno, @RequestBody WebReply reply) {
+		
+		log.info("========================================");
+		log.info("modifyReply : " + reply);
+		log.info("========================================");
+		
+		replyrepos.findById(reply.getRno()).ifPresent(origin -> {
+			
+			origin.setReplyText(reply.getReplyText());
+			
+			replyrepos.save(origin);
+			
+		});
+		
+		WebBoard board = new WebBoard();
+		board.setBno(bno);
+		
+		return new ResponseEntity<>(getListByBoard(board), HttpStatus.CREATED);
+		
+	}
+	
+	// 댓글 목록
+	@GetMapping("/{bno}")
+	public ResponseEntity<List<WebReply>> getReplies(@PathVariable("bno") Long bno) {
+		
+		log.info("get All Replies...............");
+		
+		WebBoard board = new WebBoard();
+		board.setBno(bno);
+		
+		return new ResponseEntity<>(getListByBoard(board), HttpStatus.OK);
+		
+	}
 
-	// 댓글 리스트 조회
+	// 댓글 리스트 조회 메소드
 	private List<WebReply> getListByBoard(WebBoard board) throws RuntimeException {
 		
 		log.info("getListByBoard....." + board);
