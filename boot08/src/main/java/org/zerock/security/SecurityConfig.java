02305d404dce5d7f94c28af9332e64c5fd487d14
ibk01161 <rkxmsdk@naver.com>
2020-10-05 23:48:28 +0900

    Add Proejct [boot8]_2

---
+++ boot08/src/main/java/org/zerock/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package org.zerock.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.extern.java.Log;
+
+@Log
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		log.info("security config................");
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		
+		return new PasswordEncoder() {
+			
+			@Override
+			public String encode(CharSequence rawPassword) {
+				return rawPassword.toString();
+			}
+			
+			@Override
+			public boolean matches(CharSequence rawPassword, String encodedPassword) {
+				return rawPassword.equals(encodedPassword);
+			}
+			
+		};
+		
+	}
+
+}
