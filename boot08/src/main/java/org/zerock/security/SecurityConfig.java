97b979a544b0bc929b258b12cc24a073bc572468
ibk01161 <rkxmsdk@naver.com>
2020-10-08 23:49:12 +0900

    Add Project [boot8]_55

--- boot08/src/main/java/org/zerock/security/SecurityConfig.java
+++ boot08/src/main/java/org/zerock/security/SecurityConfig.java
@@ -1,6 +1,8 @@
 package org.zerock.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,9 +14,25 @@ import lombok.extern.java.Log;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	// 인증에 대한 처리
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		
+		log.info("build Auth global............");
+		
+		auth.inMemoryAuthentication().withUser("manager").password("1111").roles("MANAGER");
+		
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		log.info("security config................");
+		
+		http.authorizeRequests().antMatchers("/guest/**").permitAll();
+		http.authorizeRequests().antMatchers("/manager/**").hasRole("MANAGER");
+		
+		// 권한이 없으면 로그인 페이지로
+		http.formLogin();
 	}
 	
 	@Bean
